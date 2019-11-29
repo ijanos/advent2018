97b159a9c24b1ca474ac5556e6a5daa9b930a36b
János Illés <ijanos@gmail.com>
2019-11-29 08:04:39 +0100

    golf rust day05

--- rust/day05/src/main.rs
+++ rust/day05/src/main.rs
@@ -1,35 +1,33 @@
 use std::io;
 use std::io::prelude::*;
-use std::collections::VecDeque;
 use std::collections::HashSet;
 
 fn reactable(p1: char, p2: char) -> bool {
     p1 != p2 &&  p1.to_ascii_lowercase() == p2.to_ascii_lowercase()
 }
 
-fn react<'a>(polymer: impl Iterator<Item = &'a char>) -> Vec<char> {
-    let mut finished: VecDeque<char> = VecDeque::new();
-    for p in polymer {
-        if reactable(*finished.back().unwrap_or(&'_'), *p) {
-            finished.pop_back();
-        } else {
-            finished.push_back(*p);
+fn react<'a>(polymer: impl Iterator<Item = &'a char>) -> usize {
+    let mut finished: Vec<char> = Vec::new();
+    for p1 in polymer {
+        match finished.last() {
+            Some(p2) if reactable(*p1, *p2) => { finished.pop(); },
+            _ => { finished.push(*p1); }
         }
     }
-    finished.into()
+    finished.len()
 }
 
 fn main() {
     let stdin = io::stdin();
     let mut stdin = stdin.lock().lines();
     let input: Vec<char> = stdin.next().unwrap().unwrap().chars().collect();
-    println!("Part 1: {}", react(input.iter()).len());
+    println!("Part 1: {}", react(input.iter()));
 
     let alphabet = input.iter().map(|c| c.to_ascii_lowercase()).collect::<HashSet<char>>();
     let mut part2 = std::usize::MAX;
     for &letter in &alphabet {
         let reduced_input = input.iter().filter(|c| c.to_ascii_lowercase() != letter);
-        let len = react(reduced_input).len();
+        let len = react(reduced_input);
         part2 = if len < part2 { len } else { part2 };
     }
     println!("Part 2: {}", part2);
